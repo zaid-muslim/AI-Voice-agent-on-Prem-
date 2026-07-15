ad55baebddd75150aef86c6d02b18bf924e930a4
abdullahamin12 <abdullahamin12570@gmail.com>
2026-07-15 12:11:07 +0500

    chore: remove unwanted folders from remote repository

--- hospital_agent/src/README.md
+++
@@ -1,1 +0,0 @@
-# Voice-agent-on-prem
--- tests/check.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-tokenizer = AutoTokenizer.from_pretrained("./models/gemma-4-E4B-it")
-model = AutoModelForCausalLM.from_pretrained("./models/gemma-4-E4B-it")
